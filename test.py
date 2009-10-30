65c3d673e5a73610439a8b20c4e23834feb8ffd7
Andrei Savu <savu.andrei@gmail.com>
2009-10-30 19:48:15 +0200

    moved tests to a single file

--- test.py
+++ test.py
@@ -1,6 +1,21 @@
 
 import unittest
-from tests.unit import *
+
+from dbgrep import any_match, any_match_set
+
+class TestRexp(unittest.TestCase):
+    
+    def testAnyMatch_singleValue(self):
+        self.assertTrue(any_match(['\d+$', 'asd'], '123'))
+
+    def testNoMatch_singleValue(self):
+        self.assertFalse(any_match(['\d+$', 'asd'], 'dgjoi'))
+
+    def testAnyMatchSet(self):
+        self.assertTrue(any_match_set(['\d+$'], ['sdf', '345']))
+
+    def testAnyNoMatchSet(self):
+        self.assertFalse(any_match_set(['\d+$'], ['df', 'dfg']))
 
 if __name__ == '__main__':
     unittest.main()    
--- tests/unit/__init__.py
+++
@@ -1,7 +0,0 @@
-
-import sys
-import os
-sys.path.append(os.path.join('..', '..'))
-
-from test_rexp import *
-
